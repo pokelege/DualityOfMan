76f274922ccf4de3d538c600d32cc0a255b5fcd4
Karl Thompson <kthompson@student.neumont.edu>
2014-09-13 13:43:30 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/team806_indiegamejam@33 57428110-b9b6-3e40-a3a8-74e02d9a4067

---
+++ Assets/Code/Elevator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+    private bool collided = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collided = true;
+    }
+
+    public bool GetTriggered()
+    {
+        return collided;
+    }
+}
---
+++ Assets/Code/LevelOneHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelOneHandler : MonoBehaviour {
+
+    public GameObject rigidElevator;
+    public GameObject curvedElevator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(rigidElevator.GetComponent<Elevator>().GetTriggered() && curvedElevator.GetComponent<Elevator>().GetTriggered())
+        {
+            LoadNextLevel();
+        }
+	}
+
+    void LoadNextLevel()
+    {
+        Application.LoadLevel(1);
+    }
+}
