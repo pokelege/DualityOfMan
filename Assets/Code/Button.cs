6366dafd3812bf3e7e77ed676ec943d370702dbb
Andrew Huertas <ahuertas@student.neumont.edu>
2014-09-13 15:15:42 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/team806_indiegamejam@37 57428110-b9b6-3e40-a3a8-74e02d9a4067

---
+++ Assets/Code/Button.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+	public bool pressed = false;
+	public GameObject up, down;
+	// Use this for initialization
+	void Start()
+	{
+		up.SetActive( true );
+		down.SetActive( false );
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		pressed = true;
+		up.SetActive( false );
+		down.SetActive( true );
+	}
+}
