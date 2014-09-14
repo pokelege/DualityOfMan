99751043b95a635cfe42b1c1dc4a4d9e27956833
Andrew Huertas <ahuertas@student.neumont.edu>
2014-09-14 02:16:46 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/team806_indiegamejam@71 57428110-b9b6-3e40-a3a8-74e02d9a4067

--- Assets/Code/Button.cs
+++ Assets/Code/Button.cs
@@ -5,22 +5,76 @@ public class Button : MonoBehaviour
 {
 	public bool pressed = false;
 	public GameObject up, down;
+	public GameObject[] elligibleObjects;
+	public bool releaseable;
+	private class ObjectChecker { public GameObject elligibleObject; public bool collided;}
+	private ObjectChecker[] objects;
 	// Use this for initialization
 	void Start()
 	{
 		up.SetActive( true );
 		down.SetActive( false );
+		if (elligibleObjects != null )
+		{
+			objects = new ObjectChecker[elligibleObjects.Length];
+			for(int i = 0; i <objects.Length; i++)
+			{
+				objects[i] = new ObjectChecker { elligibleObject = elligibleObjects[i], collided = false };
+			}
+		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if ( pressed )
+		{
+			up.SetActive( false );
+			down.SetActive( true );
+		}
+		else
+		{
+			up.SetActive( true );
+			down.SetActive( false );
+		}
+	}
+	void OnCollisionStay(Collision collision)
+	{
+		if(elligibleObjects != null && elligibleObjects.Length > 0)
+		{
+			pressed = false;
+			foreach(ObjectChecker i in objects )
+			{
+				if(collision.collider.gameObject.Equals(i.elligibleObject))
+				{
+					i.collided = true;
+					pressed = true;
+				}
+			}
+		}
+		else
+		{
+			pressed = true;
+		}
 
 	}
-	void OnCollisionEnter(Collision collision)
+
+	void OnCollisionExit(Collision collision)
 	{
-		pressed = true;
-		up.SetActive( false );
-		down.SetActive( true );
+		if ( releaseable && elligibleObjects != null && elligibleObjects.Length > 0 )
+		{
+			pressed = false;
+			foreach ( ObjectChecker i in objects )
+			{
+				if ( collision.collider.gameObject.Equals( i.elligibleObject ) )
+				{
+					i.collided = false;
+				}
+				else if(i.collided)
+				{
+					pressed = true;
+				}
+			}
+		}
 	}
 }
